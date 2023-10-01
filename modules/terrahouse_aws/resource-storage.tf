165f2d9a60f70bb44c7a3f446edd3836d79055fa
ginick git <nickgyorfi@gmail.com>
2023-10-01 18:04:10 +0000

    CDN-Implementation

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -21,19 +21,19 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
-  source = var.index_html_filepath
+  source = "${path.root}/public/index.html"
   content_type = "text/html"
 
-  etag = filemd5(var.index_html_filepath)
+  #etag = filemd5(var.index_html_filepath)
 }
 
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
-  source = var.error_html_filepath
+  source = "${path.root}/public/error.html"
   content_type = "text/html"
 
-  etag = filemd5(var.error_html_filepath)
+  #etag = filemd5(var.error_html_filepath)
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
